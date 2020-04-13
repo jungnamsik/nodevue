825a4e6bc37c52cfeeacc77418f0baea8756078b
jungnamsik <nsjung74@gmail.com>
2020-04-13 21:04:24 +0900

    vue sample

--- vue/hello/src/App.vue
+++ vue/hello/src/App.vue
@@ -1,45 +1,12 @@
 <template>
   <div id="app">
-    <h1>AAAAAAAAAAAAAAAAAAAAa</h1>
-    <h1>{{ttt}} : <span v-if="isVisible"> {{visit}}</span></h1>
-    <h2>{{tttVisit}}</h2>
-      <button @click="visitCount()">Visit count
-      </button>
-
-    <input type="text" v-model="ttt">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-  ,data() {
-    return {
-      ttt : 'jns'
-      ,visit:0
-      ,isVisible: false
-    }
-  }
-  ,methods: {
-    visitCount() {
-      this.visit ++;
-      this.isVisible = (this.visit%2 == 0) ? true : false ;
-    }
-  }
-  ,computed :{
-    tttVisit() {
-      return "Name "+this.ttt + " And Visit Count : "+ this.visit+" " ;
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -48,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ vue/hello/src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <todo-item ></todo-item>
+    <ul id="example-1">
+      <li v-for="x in items" v-bind:key="x">
+        {{ x.message }}
+      </li>
+    </ul>
+
+      <span v-for="x in 10" v-bind:key="x">
+        {{ x }}
+      </span>
+
+      
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import TodoItem from '@/components/todo-item'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+    ,TodoItem
+  }
+  ,data ()  {
+    return {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ]
+      , msg :"aaaaa"
+    }
+  }
+}
+</script>
